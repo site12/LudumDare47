e6c97fa94960ecda93f7f1aa6d1b17aa89067d86
jkazuk <jeffreykazuk@gmail.com>
2020-10-03 22:14:18 -0500

    fixed looping

--- main.gd
+++ main.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 
 func _on_End_body_entered(body):
-	$Level.position.x += 3072
+	$Level.position.x += 12288
 
 func _on_Back_body_entered(body):
-	$Level.position.x -= 3072
+	$Level.position.x -= 12288
