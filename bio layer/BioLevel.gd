3695a8e716a475a06fe2078a33d4ca42f22eb6d0
jkazuk <jeffreykazuk@gmail.com>
2020-10-05 13:37:44 -0500

    Further Updated Bio Level

--- bio layer/BioLevel.gd
+++ bio layer/BioLevel.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 func _on_End_body_entered(body):
-	position.x += 20480
+	if body.name == 'player':
+		position.x += 20480
 
 func _on_Back_body_entered(body):
-	position.x -= 20480
+	if body.name == 'player':
+		position.x -= 20480
--- ice layer/ice_level.gd
+++ ice layer/ice_level.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 
 func _on_End_body_entered(body):
-	position.x += 16384
+	if body.name == 'player':
+		position.x += 16384
 
 func _on_Back_body_entered(body):
-	position.x -= 16384
+	if body.name == 'player':
+		position.x -= 16384
