9e9d84d4b13363eeec6a653771ec1ab315c01abc
jkazuk <jeffreykazuk@gmail.com>
2020-10-05 13:15:34 -0500

    Working on full bio level

---
+++ bio layer/BioLevel.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _on_End_body_entered(body):
+	position.x += 20480
+
+func _on_Back_body_entered(body):
+	position.x -= 20480
---
+++ ice layer/ice_level.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+
+func _on_End_body_entered(body):
+	position.x += 16384
+
+func _on_Back_body_entered(body):
+	position.x -= 16384
