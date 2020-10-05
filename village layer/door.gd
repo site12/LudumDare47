c948cd554d6422039267daf0a47b9c75baf3fb50
jkazuk <jeffreykazuk@gmail.com>
2020-10-05 15:34:27 -0500

    Update door.gd

--- village layer/door.gd
+++ village layer/door.gd
@@ -31,7 +31,7 @@ func _input(event):
 		var new_scene = load(scene).instance()
 		root.add_child(new_scene)
 		new_scene.position = root.get_node(point).get_global_position()
-		root.get_node('player').current_zone = to
+		root.get_node('player').change_zone(to)
 		if to == 'village':
 			to = 'village/villagechunk2'
 		#root.get_node('player').set_deferred('position', root.get_node(to).get_node('DoorHole').global_position)
