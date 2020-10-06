33bb6a51135a2689d3c627829e16912b17f3858b
Shane <shanescott86@gmail.com>
2020-10-05 21:32:58 -0500

    menu shit

--- main.gd
+++ main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 func move_world(amount):
-    for node in get_children():
-        if node.name != 'player':
-            node.position.x += amount
+	for node in get_children():
+		if node.name != 'player':
+			node.position.x += amount
